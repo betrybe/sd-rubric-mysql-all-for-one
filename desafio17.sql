5fd7c61ddec50c888c1a1a49b8fccd11149b919d
Renato Filho <renatosousafilho@gmail.com>
2020-08-18 19:14:17 -0300

    all requirements done

---
+++ desafio17.sql
@@ -0,0 +1,3 @@
+-- Mostre os registros das colunas id e supplier_id das purchase_orders em que os supplier_id sejam tanto 1, ou 3, ou 5, ou 7.
+
+select id, supplier_id FROM purchase_orders where supplier_id in (1,3,5,7);
---
+++ desafio27.sql
@@ -0,0 +1,3 @@
+-- Delete todos os dados da tabela order_details.
+
+TRUNCATE order_details;
