5ff767dd8eccbb2a732c0c360cad1095b7ff1052
Rodrigo G. Oliveira <rodrigo.gargani@betrybe.com>
2020-12-10 21:53:00 -0300

    fix(/tests): Possivel correçao na utilizaçao nos testes locais em maquinas especificas

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,1 @@
-SELECT * FROM products;
-
-
+SELECT product_name FROM northwind.products;
--- tests/initialChallenges.spec.js
+++ tests/initialChallenges.spec.js
@@ -1,98 +1,87 @@
 const { readFileSync } = require('fs');
 const { Sequelize } = require('sequelize');
-const Importer = require('mysql-import');
+const {credentials, importer, runQuery} = require('./helper');
 
 describe('Desafios iniciais', () => {
   let sequelize;
-
+  
   beforeAll(async () => {
-    const importer = new Importer(
-      { user: process.env.MYSQL_USER, password: process.env.MYSQL_PASSWORD, host: process.env.HOSTNAME }
-    );
-
-    await importer.import('./northwind.sql');
-
-    importer.disconnect();
+    const { user, password, host, port, database } = credentials;
+    jest.setTimeout(30000);
+    
+    await importer(database);
 
     sequelize = new Sequelize(
-      `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.HOSTNAME}:3306/northwind`
+      `mysql://${user}:${password}@${host}:${port}/${database}`, { logging:false }
     );
+
+    try {
+      await sequelize.authenticate();
+    } catch (error) {
+      console.error('Could not connect to the database:', error.message);
+    }
   });
 
   afterAll(async () => {
-    await sequelize.query('DROP DATABASE northwind;', { type: 'RAW' });
+    const { database } = credentials;
+    await runQuery(sequelize, `DROP DATABASE ${database};`, { type: 'RAW' });
     sequelize.close();
+    await importer(database);
   });
 
-  describe('Exiba apenas os nomes do produtos na tabela `products`', () => {
-    it('Verifica o desafio1', async () => {
-      const challengeQuery = readFileSync('desafio1.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult1');
+  it('1: Exiba apenas os nomes do produtos na tabela `products`', async () => {
+    const challengeQuery = readFileSync('desafio1.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult1');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 
-  describe('Exiba os dados de todas as colunas da tabela `products`', () => {
-    it('Verifica o desafio2', async () => {
-      const challengeQuery = readFileSync('desafio2.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult2');
+  it('2: Exiba os dados de todas as colunas da tabela `products`', async () => {
+    const challengeQuery = readFileSync('desafio2.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult2');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 
-  describe('Escreva uma query que exiba os valores da coluna que representa a primary key da tabela `products`', () => {
-    it('Verifica o desafio3', async () => {
-      const challengeQuery = readFileSync('desafio3.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult3');
+  it('3: Escreva uma query que exiba os valores da coluna que representa a primary key da tabela `products`', async () => {
+    const challengeQuery = readFileSync('desafio3.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult3');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 
-  describe('Conte quantos registros existem em `product_name` de `products`', () => {
-    it('Verifica o desafio4', async () => {
-      const challengeQuery = readFileSync('desafio4.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult4');
+  it('4: Conte quantos registros existem em `product_name` de `products`', async () => {
+    const challengeQuery = readFileSync('desafio4.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult4');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 
-  describe('Monte uma query que exiba os dados da tabela `products` a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro', () => {
-    it('Verifica o desafio5', async () => {
-      const challengeQuery = readFileSync('desafio5.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult5');
+  it('5: Monte uma query que exiba os dados da tabela `products` a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro', async () => {
+    const challengeQuery = readFileSync('desafio5.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult5');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 
-  describe('Exiba os dados das colunas `product_name` e `id` da tabela `products` de maneira que os resultados estejam em ordem alfabética dos nomes', () => {
-    it('Verifica o desafio6', async () => {
-      const challengeQuery = readFileSync('desafio6.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult6');
+  it('6: Exiba os dados das colunas `product_name` e `id` da tabela `products` de maneira que os resultados estejam em ordem alfabética dos nomes', async () => {
+    const challengeQuery = readFileSync('desafio6.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult6');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 
-  describe('Mostre apenas os ids dos 5 últimos registros da tabela `products` (a ordernação deve ser baseada na coluna `id`)', () => {
-    it('Verifica o desafio7', async () => {
-      const challengeQuery = readFileSync('desafio7.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult7');
+  it('7: Mostre apenas os ids dos 5 últimos registros da tabela `products` (a ordernação deve ser baseada na coluna `id`)', async () => {
+    const challengeQuery = readFileSync('desafio7.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult7');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 
-  describe('Faça uma consulta que retorne três colunas contendo os nomes `A`, `Trybe` e `eh` com os valores `5 + 6`, `"de"` e `2 + 8`, respectivamente', () => {
-    it('Verifica o desafio8', async () => {
-      const challengeQuery = readFileSync('desafio8.sql', 'utf8').trim();
-      const expectedResult = require('./challengesResults/challengeResult8');
+  it('8: Faça uma consulta que retorne três colunas contendo os nomes `A`, `Trybe` e `eh` com os valores `5 + 6`, `"de"` e `2 + 8`, respectivamente', async () => {
+    const challengeQuery = readFileSync('desafio8.sql', 'utf8').trim();
+    const expectedResult = require('./challengesResults/challengeResult8');
 
-      expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
-    });
+    expect(await runQuery(sequelize, challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
   });
 });
